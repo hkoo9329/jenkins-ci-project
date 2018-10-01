94ba3e65c23a495dbacf68153509881846ba2a48
otter3552 <mr_shield@naver.com>
2018-10-01 19:09:23 +0900

    Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+package com.jenkins.ci.project;
+import java.util.Scanner;
+public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+		
+		 int x;
+		 int y;
+		 x=in.nextInt();
+		 y=in.nextInt();
+		 
+		System.out.println(x+y);
+		
+	}
+	
+
+}
