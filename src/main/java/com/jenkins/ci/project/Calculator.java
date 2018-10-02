f20c8a5a8281f7dac5b0fc09acd44ceac0e2f27d
leejuhyeok <tyrfingr12@naver.com>
2018-10-02 22:45:18 +0900

    update

--- src/main/java/com/jenkins/ci/project/Calculator.java
+++ src/main/java/com/jenkins/ci/project/Calculator.java
@@ -1,10 +1,23 @@
 package com.jenkins.ci.project;
+import java.util.Scanner;
+public class main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+		
+		 int x;
+		 int y;
+		 x=in.nextInt();
+		 y=in.nextInt();
+		 
+		System.out.println(x+y);
+		
+	}
+	
 
-
-public class Calculator
-{
-    public static void main( String[] args )
-    {
-     // 코드 작성란
-    }
 }
+
+
+
+
+
